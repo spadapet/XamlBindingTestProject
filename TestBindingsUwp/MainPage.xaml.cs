9375522ed509f1a6b628f42594952470040df73d
Peter Spada <peterspa@microsoft.com>
2020-02-18 13:19:44 -0800

    Add UWP project

---
+++ TestBindingsUwp/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace TestBindingsUwp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainVM ViewModel { get; }
+
+        public MainPage()
+        {
+            this.ViewModel = new MainVM();
+            this.InitializeComponent();
+        }
+    }
+}
