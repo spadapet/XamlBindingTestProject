f7d51742d3823fc9a1aa6a34b905a3612ee3bf57
Peter Spada <peterspa@microsoft.com>
2020-06-15 10:56:09 -0700

    UWP Clear people

--- TestBindingsUwp/MainPage.xaml.cs
+++ TestBindingsUwp/MainPage.xaml.cs
@@ -12,10 +12,16 @@ namespace TestBindingsUwp
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private void ButtonClickAdd(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             System.Diagnostics.Debug.WriteLine("Adding a person");
             this.ViewModel.People.Add(new PersonVM() { Name = "Foobar" });
+        }
+
+        private void ButtonClickClear(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Clearing pepole");
+            this.ViewModel.People.Clear();
         }
     }
 }
